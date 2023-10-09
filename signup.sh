d2f5cdbdc237774cb45ba7511d064b9d4952ae35
Fernando Giaretta <fgiaretta42@gmail.com>
2023-10-09 03:41:10 -0300

    Add flow auth to bash script

--- signup.sh
+++ signup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Set the endpoint URL
-ENDPOINT_URL="https://la1j3jdzu1.execute-api.us-east-1.amazonaws.com/dev/user/signup"
+ENDPOINT_URL="https://la1j3jdzu1.execute-api.us-east-1.amazonaws.com/dev/user"
 
+# Set email and password variables
 EMAIL="fgiaretta42@gmail.com"
-PASSWORD="password123"
+PASSWORD="paSsword123#"
+CODE="875202"
 
-curl -X POST \
-    -H "Content-Type: application/json" \
-    -d "{"email":"$EMAIL","password":"$PASSWORD"}" \
-    $ENDPOINT_URL
+# Signup
+curl -X POST -H "Content-Type: application/json" -d "{\"email\":\"$EMAIL\",\"password\":\"$PASSWORD\"}" $ENDPOINT_URL/signup
+
+# Login
+curl -X POST -H "Content-Type: application/json" -d "{\"email\":\"$EMAIL\",\"password\":\"$PASSWORD\"}" $ENDPOINT_URL/login
+
+# Confirm
+curl -X POST -H "Content-Type: application/json" -d "{\"email\":\"$EMAIL\",\"code\":\"$CODE\"}" $ENDPOINT_URL/confirm
